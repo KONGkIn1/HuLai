5c7087ca6e5f85b0689c427a54e5761de8fc36cc
Kong <2173508828@qq.com>
2026-07-27 16:24:14 +0800

    工作台业务代码

--- sky-take-out/sky-server/src/main/java/com/sky/task/WebSocketTask.java
+++ sky-take-out/sky-server/src/main/java/com/sky/task/WebSocketTask.java
@@ -13,9 +13,9 @@ public class WebSocketTask {
     private WebSocketServer webSocketServer;
 
     /**
-     * 通过WebSocket每隔5秒向客户端发送消息
+     * 通过WebSocket每隔5秒向客户端发送消息（测试用，上线前注释掉）
      */
-    @Scheduled(cron = "0/5 * * * * ?")
+    // @Scheduled(cron = "0/5 * * * * ?")
     public void sendMessageToClient() {
         webSocketServer.sendToAllClient("这是来自服务端的消息：" + DateTimeFormatter.ofPattern("HH:mm:ss").format(LocalDateTime.now()));
     }
